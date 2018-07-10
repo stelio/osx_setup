f6c4a201d5dc657e6224f8ec32f46b01f3147edd
Stelio Gouveia <stelio.gouveia@gmail.com>
2018-07-10 13:01:44 +0200

    Added minikube

---
+++ install_minikube.sh
@@ -0,0 +1,3 @@
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.0/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+minikube start --vm-driver=xhyve
