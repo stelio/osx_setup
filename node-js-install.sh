9e0f47608926d75b22cd519e25e7d3c38071173e
Stelio Gouveia <stelio.gouveia@gmail.com>
2022-05-31 14:06:51 +0200

    Update node-js-install.sh

--- node-js-install.sh
+++ node-js-install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-
-source ~/.bash_profile
-
-nvm install node
-nvm install v6.11.1
-nvm use v6.11.1
-nvm alias default v6.11.1
+brew install nvm
+nvm install 10.14.2
+nvm use 10.14.2
+nvm ls
+nvm current
+nvm alias default 10.14.2
