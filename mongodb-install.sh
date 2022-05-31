d20d5853fe5103feca1cdfd0fbfb43da4f4fd557
Stelio Gouveia <stelio.gouveia@gmail.com>
2022-05-31 15:08:49 +0200

    Update mongodb-install.sh

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -6,8 +6,12 @@
 # - mongodb@3.2
 # - mongodb@3.4
 
-brew install mongodb@3.2
+# Instructions for Mongodb 3.2
+# $ brew install mongodb@3.2
+# $ echo 'export PATH="/usr/local/opt/mongodb@3.2/bin:$PATH"' >> ~/.bash_profile
+# $ brew services start mongodb@3.2
 
-echo 'export PATH="/usr/local/opt/mongodb@3.2/bin:$PATH"' >> ~/.bash_profile
-
-brew services start mongodb@3.2
+brew tap mongodb/brew
+brew install mongodb-community@4.2
+echo 'export PATH="/usr/local/opt/mongodb-community@4.2/bin:$PATH"' >> ~/.zshrc
+brew services start mongodb-community@4.2
