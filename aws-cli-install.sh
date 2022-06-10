2adace689e02bdf511d1f855d61c7f4ceeda0d5f
Stelio Gouveia <stelio.gouveia@gmail.com>
2022-06-10 15:12:02 +0200

    Update aws-cli-install.sh

--- aws-cli-install.sh
+++ aws-cli-install.sh
@@ -5,3 +5,8 @@ sudo installer -pkg AWSCLIV2.pkg -target /
 which aws
 
 aws --version
+
+# Install EB toolkit
+git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
+python ./aws-elastic-beanstalk-cli-setup/scripts/ebcli_installer.py
+echo 'export PATH="/Users/stelio/.ebcli-virtual-env/executables:$PATH"' >> ~/.zshenv && source ~/.zshenv
