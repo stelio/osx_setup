d56b73053a0bd28517ba822ccbe4520f85d0a5d5
Stelio Gouveia <stelio.gouveia@gmail.com>
2022-06-10 16:53:14 +0200

    Update python3-install.sh

--- python3-install.sh
+++ python3-install.sh
@@ -1,17 +1,20 @@
 PYTHON_VERSION='3.7'
 
-brew install python3
-brew install python@$PYTHON_VERSION
-# Alternatively:
-# ibrew install python@$PYTHON_VERSION
-brew link --force python@$PYTHON_VERSION
-brew info python@$PYTHON_VERSION
+# x86 instructions
+#brew install python3
+#brew install python@$PYTHON_VERSION
+#brew link --force python@$PYTHON_VERSION
+#brew info python@$PYTHON_VERSION
+#echo 'export PATH="/Users/'$USER'/Library/Python/'$PYTHON_VERSION'/bin:$PATH"' >> ~/.zshrc
 
-echo 'alias python=/usr/bin/python3' >> ~/.zshrc
-echo 'alias pip=/usr/bin/pip3' >> ~/.zshrc
+# M1 Rosetta instructions
+ibrew install python@$PYTHON_VERSION
+ibrew link --force python@$PYTHON_VERSION
+
+echo 'export PATH="/usr/local/opt/python@'$PYTHON_VERSION'/bin:$PATH"' >> ~/.zshrc
+echo 'alias python=python3' >> ~/.zshrc
+echo 'alias pip=pip3' >> ~/.zshrc
 
 pip install virtualenv
-
-echo 'export PATH="/Users/'$USER'/Library/Python/'$PYTHON_VERSION'/bin:$PATH"' >> ~/.zshrc
 
 source ~/.zshrc
