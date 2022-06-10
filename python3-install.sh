62d863eb3faa0a2e04adef6ab7e08c2420afe676
Stelio Gouveia <stelio.gouveia@gmail.com>
2022-06-10 14:57:52 +0200

    Update python3-install.sh

--- python3-install.sh
+++ python3-install.sh
@@ -1,6 +1,14 @@
-brew install python3
+PYTHON_VERSION='3.8'
 
-# brew link python@3.9
+brew install python3
+brew link --force python@$PYTHON_VERSION
+brew info python@$PYTHON_VERSION
 
 echo 'alias python=/usr/bin/python3' >> ~/.zshrc
 echo 'alias pip=/usr/bin/pip3' >> ~/.zshrc
+
+pip install virtualenv
+
+echo 'export PATH="/Users/'$USER'/Library/Python/'$PYTHON_VERSION'/bin:$PATH"' >> ~/.zshrc
+
+source ~/.zshrc
