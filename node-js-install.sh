cea1c66bf643780013a0e85eefa4186abc9e483a
Stelio Gouveia <stelio.gouveia@gmail.com>
2025-05-09 09:58:12 +0200

    Update node-js-install.sh

--- node-js-install.sh
+++ node-js-install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 brew install nvm
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.profile
+echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.profile
+echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.profile
+
+source ~/.profile
+
 nvm install 22.14.0
 nvm use 22.14.0
 nvm ls
